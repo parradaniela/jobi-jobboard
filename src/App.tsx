241da98c9165b3d1176fa9844506f1501382d533
DanielaParra <dparralp396@gmail.com>
2023-03-21 15:28:08 -0400

    Setting up Router provider with two Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider
+} from "react-router-dom";
+//layouts
+import RootLayout from "./components/layouts/RootLayout";
+// pages
 import Home from "./components/Home/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import JobList from "./components/JobList/JobList";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}> 
+      <Route index element={<Home />} />
+      <Route path="job-list" element={<JobList />} />
+      {/* <Route path="/error" element={<Error />} /> */}
+    </Route>
+  )
+)
 
 function App() {
   
   return (
-    <BrowserRouter> 
-      <Routes> 
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/job-list" element={<JobList />} />
-        <Route path="/error" element={<Error />} /> */}
-      </Routes>
-    </BrowserRouter>
+    
+    <RouterProvider router={router} />
+    
   )
 }
 
