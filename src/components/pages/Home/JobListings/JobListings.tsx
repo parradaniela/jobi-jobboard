7a44d7a589b315a3c9a2626b3603b76a80670dad
Daniela Parra <dparralp396@gmail.com>
2023-03-28 14:19:41 -0400

    Updated styles for aside at bottom of section

--- src/components/pages/Home/JobListings/JobListings.tsx
+++ src/components/pages/Home/JobListings/JobListings.tsx
@@ -12,7 +12,7 @@ const JobListings = () => {
     return (
         <section className="wrapper">
             <div className="flex justify-between items-center py-8">
-                <h2 className="font-bold text-2xl">New Job Listings</h2>
+                <h2 className="text-6xl">New Job Listings</h2>
                 <NavLink to="job-list" className="text-jobi-green-bright py-2 border-b-2 border-b-jobi-green-bright transition-all hover:text-jobi-green-dark hover:border-b-jobi-green-dark focus:text-jobi-green-dark focus:border-b-jobi-green-dark">Explore all jobs &rsaquo;</NavLink>
             </div>
             <ul className="flex flex-col gap-4 justify-center items-center py-4">
@@ -38,11 +38,11 @@ const JobListings = () => {
                     })
                 }
             </ul>
-            <aside>
-                <p>
-                    Do you want to post a job for your company? 
-                    <span>
-                        We can help. <NavLink to="error">Click here</NavLink>
+            <aside className="text-center">
+                <p className="border-2 border-black inline-block py-1 px-3 rounded-full">
+                    Do you want to post a job for your company?&nbsp;
+                    <span className="text-jobi-green-bright font-semibold">
+                        We can help. <NavLink to="error" className="font-normal underline hover:text-jobi-green-dark focus:text-jobi-green-dark transition-colors">Click here</NavLink>
                     </span>
                 </p>
             </aside>
