75a4bcb7ddbdb263b88333f570595824dfff1465
Daniela Parra <dparralp396@gmail.com>
2023-03-27 20:17:29 -0400

    Finished home page job list markup

---
+++ src/components/pages/Home/JobListings/JobListItem.tsx
@@ -0,0 +1,28 @@
+import { JobData } from "../../../../types/databaseTypes"
+import Button from "../../../Global/Buttons/Button"
+
+const JobListItem = ({title, type, date, company, country, city, categories}: JobData) => {
+    return (
+        <li className="flex">
+            <h3>{title}</h3>
+            <div>
+                <p>{type}</p>
+                <p>{date} by <span>{company}</span></p>
+            </div>
+            <div>
+                <p>{country}, {city}</p>
+                {
+                    categories.map(category => (
+                        <p key={category}>{category}, </p>
+                    ))
+                }
+            </div>
+            <div>
+                <button>Bookmark</button>
+                <Button style={"green"} corners={"rounded"}>Apply</Button>
+            </div>
+        </li>
+    )
+}
+
+export default JobListItem
--- src/components/pages/Home/JobListings/JobListings.tsx
+++ src/components/pages/Home/JobListings/JobListings.tsx
@@ -1,18 +1,40 @@
+//react imports
 import { useContext } from "react"
 import { NavLink } from "react-router-dom"
 import { DatabaseContext } from "../../../../context/DatabaseContext"
+// components
+import JobListItem from "./JobListItem"
 
 const JobListings = () => {
-    // const { jobData } = useContext(DatabaseContext)
-    // console.log(jobData)
+    const { jobData } = useContext(DatabaseContext)
+    console.log(jobData)
     return (
         <section className="wrapper">
             <div className="flex">
                 <h2>New Job Listing</h2>
                 <NavLink to="job-list">Explore all jobs</NavLink>
             </div>
-            <ul>
-                {/* Firebase data goes here */}
+            <ul className="">
+                {
+                    jobData.map(({categories, city, country, experience, salaryEnd, salaryStart, salaryType, title, type}, i) => {
+                        return (
+                            <JobListItem
+                                key={i} //TODO: Add ID numbers to jobs to use as keys (and routes)
+                                categories={categories}
+                                city={city}
+                                country={country}
+                                company={"test"}
+                                date={"today"}
+                                experience={experience}
+                                salaryEnd={salaryEnd}
+                                salaryStart={salaryStart}
+                                salaryType={salaryType}
+                                title={title}
+                                type={type}
+                            />
+                        )
+                    })
+                }
             </ul>
             <aside>
                 <p>
