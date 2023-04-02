07b96a70fcc9a8828ff466bbae7241ae575a4239
DanielaParra <dparralp396@gmail.com>
2023-04-02 15:32:10 -0400

    Fixed broken images in Partners component
    Removed PartnerImg component and map() in Partners, in favour of simply having img JSX elements in a list in Partners

--- src/components/pages/Home/Partners/Partners.tsx
+++ src/components/pages/Home/Partners/Partners.tsx
@@ -1,22 +1,22 @@
-//data
-import partners from './partners.json';
-//Component
-import PartnerImg from "./PartnerImg"
+//logos
+import googleLogo from '../../../../assets/index/logos/logo-google.png';
+import shipBobLogo from '../../../../assets/index/logos/logo-shipbob.png';
+import dribbbleLogo from '../../../../assets/index/logos/logo-dribbble.png';
+import slackLogo from '../../../../assets/index/logos/logo-slack.png';
+import vineLogo from '../../../../assets/index/logos/logo-vine.png';
+import airbnbLogo from '../../../../assets/index/logos/logo-airbnb.png';
 
 const Partners = () => {
     return (
         <section className="wrapper py-4">
             <h2 className="sr-only">Our Partners</h2>
             <ul className="flex justify-center items-center gap-10">
-                {
-                    partners.map((partner, i) => {
-                        return (
-                            <li key={i}>
-                                <PartnerImg src={partner.src} alt={`Partnered with ${partner.name}`} />
-                            </li>
-                        )
-                    })
-                }
+                <li><img src={googleLogo} alt="Partnered with Google" /></li>
+                <li><img src={shipBobLogo} alt="Partnered with ShipBob" /></li>
+                <li><img src={dribbbleLogo} alt="Partnered with Dribbble" /></li>
+                <li><img src={slackLogo} alt="Partnered with Slack" /></li>
+                <li><img src={vineLogo} alt="Partnered with Vine" /></li>
+                <li><img src={airbnbLogo} alt="Partnered with AirBnB" /></li>
             </ul>
         </section>
     )
