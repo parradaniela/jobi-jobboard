4d84652f37c8e4b13a0ac4252344fd80ea3c2143
DanielaParra <dparralp396@gmail.com>
2023-04-02 17:17:07 -0400

    Added styles to Trending section except for bg images

--- src/components/pages/Home/Trending/Trending.tsx
+++ src/components/pages/Home/Trending/Trending.tsx
@@ -4,15 +4,15 @@ import { NavLink } from "react-router-dom"
 const Trending = () => {
     const trendingLabels = ['Wordpress Development', 'Audio & Video Editing', 'Product & Branding Design', 'Admin & Customer Support']
     return (
-        <section className="wrapper">
-            <h2>Trending Jobs</h2>
-            <ul className="flex justify-center items-center gap-4">
+        <section className="wrapper font-gordita">
+            <h2 className="text-6xl text-jobi-black">Trending Jobs</h2>
+            <ul className="flex justify-center items-center gap-4 my-16">
                 {
                     trendingLabels.map(label => {
                         return (
                             <NavLink to="/" key={label}>
-                                <li>
-                                    <h3>{label}</h3>
+                                <li className="bg-slate-400 h-96 w-80 flex items-end">
+                                    <h3 className="text-lg text-white font-medium p-8 w-[73%]">{label}</h3>
                                 </li>
                             </NavLink>
                         )
