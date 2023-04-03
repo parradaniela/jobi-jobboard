7128ead843de5fe8df6fa3e9833523af545f03c3
DanielaParra <dparralp396@gmail.com>
2023-04-02 21:44:44 -0400

    Fixed font styling and moved stepcard prop types to corresponding types file

--- src/components/pages/Home/HowItWorks/StepCard.tsx
+++ src/components/pages/Home/HowItWorks/StepCard.tsx
@@ -1,22 +1,18 @@
+//react imports
 import { NavLink } from "react-router-dom"
+//type imports
+import { StepCardProps } from "../../../../types/button&LinkTypes"
 
-type PropsType = {
-    src: string,
-    text: string,
-    url: string,
-    linkText: string
-}
-
-const StepCard = ({src, text, url, linkText}: PropsType) => {
+const StepCard = ({src, text, url, linkText}: StepCardProps) => {
     return (
         <li className="flex gap-4 p-4 bg-slate-50 rounded-lg">
-            <div>
+            <div className=" w-12">
                 {/* Leaving alt text empty so the images are skipped by screen readers, they don't add anything */}
                 <img src={src} alt="" />
             </div>
             <div>
                 <p className="py-4">{text}</p>
-                <NavLink to={url} className="uppercase text-jobi-green-bright pb-4">
+                <NavLink to={url} className="uppercase text-jobi-green-bright transition-all pb-4 hover:text-jobi-green-dark hover:underline">
                     {linkText}
                 </NavLink>
             </div>
