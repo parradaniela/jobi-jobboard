272e911d162f47facb54548b6d7b3422b9e6beab
DanielaParra <dparralp396@gmail.com>
2023-04-03 18:28:30 -0400

    Initial styles for Testimonials section

--- src/components/pages/Home/Testimonials/TestimonialCarousel.tsx
+++ src/components/pages/Home/Testimonials/TestimonialCarousel.tsx
@@ -1,16 +1,21 @@
+import quotations from "../../../../assets/index/button-icons/icon-quotes.svg";
+
 const TestimonialCarousel = () => {
     return (
-        <li>
-            <div>
-                <p>
+        <li className="bg-white p-11 w-2/5 rounded-md">
+            <div className="flex items-center justify-between">
+                <h3 className="text-lg text-jobi-green-bright leading-loose">
                     Impressive!
-                    <span className="block">stars</span>
-                </p>
-                <p>99</p>
+                    <span className="block">⭐⭐⭐⭐⭐</span>
+                </h3>
+                {/* leaving alt text empty so image is skipped by screen readers */}
+                <img src={quotations} alt="" />
             </div>
-            <p>Amazing theme, I'm using it for our internal process & procedures, and it's working very well.</p>
-            <div>
-                <p>Rashed Ka, Dhaka</p>
+            <p className="text-base leading-loose py-6">Amazing theme, I'm using it for our internal process & procedures, and it's working very well.</p>
+            <div className="flex justify-between items-center py-4">
+                <p className="text-sm leading-loose">
+                    <span className="font-bold">Rashed Ka,</span> Dhaka
+                </p>
                 <img src="#" alt="photo" />
             </div>
         </li>
--- src/components/pages/Home/Testimonials/Testimonials.tsx
+++ src/components/pages/Home/Testimonials/Testimonials.tsx
@@ -1,18 +1,25 @@
+import Button from "../../../Global/Buttons/Button"
 import TestimonialCarousel from "./TestimonialCarousel"
 
 
 const Testimonials = () => {
     return (
-        <section>
+        <section className="py-8 font-gordita bg-slate-400">
             <div className="wrapper">
-                <div>
-                    <h2>What do our clients think of us?</h2>
-                    <div>
-                        <button>&larr;</button>
-                        <button>&rarr;</button>
+                <div className="flex items-end justify-between">
+                    <h2 className="text-6xl leading-normal text-white w-1/2 capitalize">What do our clients think of us?</h2>
+                    <div className="flex basis-1/12 justify-between text-white pb-4">
+                        <Button style="btn-outline" corners="rounded-full" >
+                            &larr;
+                            <span className="sr-only">Previous reviews</span>
+                        </Button>
+                        <Button style="btn-green" corners="rounded-full" >
+                            &rarr;
+                            <span className="sr-only">Next reviews</span>
+                        </Button>
                     </div>
                 </div>
-                <ul className="flex justify-between">
+                <ul className="flex justify-center items-center gap-8 py-8">
                     <TestimonialCarousel />
                     <TestimonialCarousel />
                 </ul>
