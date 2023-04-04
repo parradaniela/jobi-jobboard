62335742781c103aff57d6932b2d591f24c51c27
DanielaParra <dparralp396@gmail.com>
2023-04-03 21:54:51 -0400

    Fixed styling in footer aside component

--- src/components/Footer/Aside.tsx
+++ src/components/Footer/Aside.tsx
@@ -7,18 +7,29 @@ import ListItemLinks from '../Global/LILinks/ListItemLinks';
 
 const Aside = () => {
   return (
-    <aside className="flex justify-between text-white wrapper">
-        <ul className="flex justify-between">
-            <ListItemLinks url={'/'} text={'Privacy & Terms'} />
-            <ListItemLinks url={'/'} text={'Contact Us'} />
-            
-        </ul>
-        <p className="text-xs opacity-60">Copyright @2022 jobi inc.</p>
-        <ul className="flex justify-between">
-            <li><img src={logoFacebook} alt="Visit our Facebook page" /></li>
-            <li><img src={logoInstagram} alt="Visit our Instagram page" /></li>
-            <li><img src={logoPinterest} alt="Visit our Pinterest page" /></li>
-        </ul>
+    <aside className="pt-12 flex justify-between text-white wrapper">
+      <ul className="flex justify-between gap-4">
+        <ListItemLinks url='/' text='Privacy & Terms' extraClasses='hover:underline hover:text-jobi-green-light'/>
+        <ListItemLinks url='/' text='Contact Us' extraClasses='hover:underline hover:text-jobi-green-light'/>
+      </ul>
+      <p className="text-xs opacity-60">Copyright @2022 jobi inc.</p>
+      <ul className="flex justify-between gap-4">
+        <li className='transition-all hover:scale-150 focus:scale-150'>
+          <a href="/">
+            <img src={logoFacebook} alt="Visit our Facebook page" />
+          </a>
+        </li>
+        <li className='transition-all hover:scale-150 focus:scale-150'>
+          <a href="/">
+            <img src={logoInstagram} alt="Visit our Instagram page" />
+          </a>
+        </li>
+        <li className='transition-all hover:scale-150 focus:scale-150'>
+          <a href="/">
+            <img src={logoPinterest} alt="Visit our Pinterest page" />
+          </a>
+        </li>
+      </ul>
     </aside>
   )
 }
