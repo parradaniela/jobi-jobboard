7c8a56a67a88233bbae2938bd7441a745bfe6d20
DanielaParra <dparralp396@gmail.com>
2023-04-03 21:16:45 -0400

    First pass at styles for home page blog section

--- src/components/pages/Home/Blog/BlogHighlights.tsx
+++ src/components/pages/Home/Blog/BlogHighlights.tsx
@@ -4,12 +4,17 @@ import HighlightArticle from "./HighlightArticle"
 
 const BlogHighlights = () => {
   return (
-    <section className="wrapper">
-      <div>
-        <h2>Jobi Guides</h2>
-        <NavLink to="error">Explore More &rsaquo;</NavLink>
+    <section className="wrapper font-gordita py-8">
+      <div className="flex items-center justify-between pb-8">
+        <h2 className="text-6xl text-jobi-green-dark">Jobi Guides</h2>
+        <NavLink
+          to="/"
+          className="text-sm text-jobi-green-bright py-2 border-b-2 border-b-jobi-green-bright transition-all hover:text-jobi-green-dark hover:border-b-jobi-green-dark focus:text-jobi-green-dark focus:border-b-jobi-green-dark"
+        >
+          Explore More &rsaquo;
+        </NavLink>
       </div>
-      <div className="flex justify-evenly">
+      <div className="flex justify-evenly gap-8">
         <HighlightArticle />
         <HighlightArticle />
         <HighlightArticle />
--- src/components/pages/Home/Blog/HighlightArticle.tsx
+++ src/components/pages/Home/Blog/HighlightArticle.tsx
@@ -1,12 +1,15 @@
+import { NavLink } from "react-router-dom"
 
 
 const HighlightArticle = () => {
     return (
-        <article>
+        <article className="flex flex-col gap-4">
             <img src="" alt="articleimg" />
-            <h3>Challenge yourself and win the future</h3>
+            <p className="text-xs font-gorditaLight">22 July, 2022</p>
+            <h3 className="text-lg font-medium text-jobi-green-dark">Challenge yourself and win the future</h3>
             {/* TODO: find the property that lets you compress text with a real ... */}
-            <p>This response is important for our ability to learn from mistakes, but words ...</p>
+            <p className="text-base font-gorditaLight">This response is important for our ability to learn from mistakes, but words ...</p>
+            <NavLink to="/">&rarr;</NavLink>
         </article>
     )
 }
