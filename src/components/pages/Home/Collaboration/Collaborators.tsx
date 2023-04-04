b15d7af91c85e0bae3336f3b0ec2fe7ab08d4091
DanielaParra <dparralp396@gmail.com>
2023-04-03 21:39:24 -0400

    Styles for collaborators section in home page
    The left side div with the circles and images still needs research to successfully code

--- src/components/pages/Home/Collaboration/Collaborators.tsx
+++ src/components/pages/Home/Collaboration/Collaborators.tsx
@@ -1,16 +1,20 @@
 import { NavLink } from "react-router-dom"
-
+import jobiLogo from "../../../../assets/index/logos/logo-jobi-2.png";
 
 const Collaboration = () => {
     return (
-        <section className="wrapper">
-            <div>
-                {/* Whatever the heck's going on in this img */}
+        <section className="wrapper py-8 flex justify-center font-gordita">
+            <div className="w-1/2 border-1 border-b-gray-300 rounded-[50%] flex items-center justify-center">
+                <div className="border-1 border-b-gray-300 w-3/4 h-3/4 rounded-[50%] flex justify-center items-center">
+                    <img src={jobiLogo} alt="" />
+                    {/* What the heck's going on here - revisit later */}
+                </div>
             </div>
-            <div>
-                <h2>Collaboration with Top Brands</h2>
-                <p>We collaborate with a number of top tier companies on imagining the future of work. Have a look.</p>
-                <NavLink to="/">Learn more &rarr;</NavLink>
+            <div className="p-12 w-1/3">
+                <span className="uppercase text-gray-400 leading-loose">Top brand</span>
+                <h2 className="text-6xl py-4">Collaboration with Top Brands</h2>
+                <p className="text-base py-4 leading-loose">We collaborate with a number of top tier companies on imagining the future of work. Have a look.</p>
+                <NavLink to="/" className="text-base text-jobi-green-dark font-bold hover:text-jobi-green-bright focus:text-jobi-green-bright transition-all">Learn more &rarr;</NavLink>
             </div>
         </section>
     )
