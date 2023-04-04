f74d0fa228c1a56dba0474dfe0e5be79b8d050f1
DanielaParra <dparralp396@gmail.com>
2023-04-03 22:05:21 -0400

    Adding center justify and align classes
    Fixes bad alignment of children in live site

--- src/components/Global/Buttons/Anchor.tsx
+++ src/components/Global/Buttons/Anchor.tsx
@@ -8,7 +8,7 @@ const Anchor = ({ children, style, corners, extraClasses, url }: AnchorProps) =>
         <NavLink
             to={url}   
             className={`
-            flex gap-3 border-2 px-4 py-1.5 transition-colors
+            flex justify-center items-center gap-3 border-2 px-4 py-1.5 transition-colors
             ${style} 
             ${corners} 
             ${extraClasses ? extraClasses : ''}
