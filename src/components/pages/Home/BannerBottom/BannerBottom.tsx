7fe37666d51e38e1f25420a7c34c00402c306758
DanielaParra <dparralp396@gmail.com>
2023-04-03 21:45:23 -0400

    First pass at styles for home page bottom banner

--- src/components/pages/Home/BannerBottom/BannerBottom.tsx
+++ src/components/pages/Home/BannerBottom/BannerBottom.tsx
@@ -1,14 +1,16 @@
-import Button from "../../../Global/Buttons/Button"
+import Anchor from "../../../Global/Buttons/Anchor"
 
 const BannerBottom = () => {
     return (
-        <section className="bg-slate-500">
-            <div className="wrapper flex justify-evenly">
-                <h2>A complete job portal for everyone</h2>
-                <div>
-                    <p>Find your dream job & earn from all worldwide leading brands</p>
-                    <Button style="btn-outline" corners="rounded">Looking for a job?</Button>
-                    <Button style="btn-green" corners="rounded">Start Hiring</Button>
+        <section className="bg-slate-500 py-16 font-gordita">
+            <div className="wrapper flex justify-evenly gap-8">
+                <h2 className="text-6xl text-white w-1/2 capitalize">A complete job portal for everyone</h2>
+                <div className="py-8">
+                    <p className="text-lg text-white">Find your dream job & earn from all worldwide leading brands</p>
+                    <div className="flex justify-center items-center gap-4 py-8 ">
+                        <Anchor style="btn-outline" corners="rounded" url="/">Looking for a job?</Anchor>
+                        <Anchor style="btn-green" corners="rounded" url="/">Start Hiring</Anchor>
+                    </div>
                 </div>
             </div>
         </section>
