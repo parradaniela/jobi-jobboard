70881d82756ebe13cf94eab54a6f77bf2e55ca94
DanielaParra <dparralp396@gmail.com>
2023-04-09 14:06:00 -0400

    Fixed order of li > a elements, added hover state

--- src/components/pages/Home/Trending/Trending.tsx
+++ src/components/pages/Home/Trending/Trending.tsx
@@ -6,34 +6,34 @@ const Trending = () => {
         <section className="wrapper font-gordita">
             <h2 className="text-6xl text-jobi-black">Trending Jobs</h2>
             <ul className="flex justify-center items-center gap-4 my-16">
-                <NavLink to="/">
-                    <li className="bg-wp bg-cover bg-[5%] h-96 w-80 flex items-end">
+                <li className="bg-wp bg-cover bg-[5%] h-96 w-80 transition-all hover:bg-gradient-to-b from-slate-200 to-slate-700">
+                    <NavLink to="/" className="h-full w-full flex items-end transition-all hover:items-center hover:justify-center hover:text-center">
                         <h3 className="text-lg text-white font-medium p-8 w-[73%]">
                             Wordpress Development
                         </h3>
-                    </li>
-                </NavLink>
-                <NavLink to="/">
-                    <li className="bg-audio bg-cover bg-left h-96 w-80 flex items-end">
+                    </NavLink>
+                </li>
+                <li className="bg-audio bg-cover bg-left h-96 w-80 transition-all hover:bg-gradient-to-b from-slate-200 to-slate-700">
+                    <NavLink to="/" className="h-full w-full flex items-end transition-all hover:items-center hover:justify-center hover:text-center">
                         <h3 className="text-lg text-white font-medium p-8 w-[73%]">
                             Audio & Video Editing
                         </h3>
-                    </li>
-                </NavLink>
-                <NavLink to="/">
-                    <li className="bg-branding bg-cover h-96 w-80 flex items-end">
+                    </NavLink>
+                </li>
+                <li className="bg-branding bg-cover h-96 w-80 transition-all hover:bg-gradient-to-b from-slate-200 to-slate-700">
+                    <NavLink to="/" className="h-full w-full flex items-end transition-all hover:items-center hover:justify-center hover:text-center">
                         <h3 className="text-lg text-white font-medium p-8 w-[73%]">
                             Product & Branding Design
                         </h3>
-                    </li>
-                </NavLink>
-                <NavLink to="/">
-                    <li className="bg-support bg-cover bg-[30%] h-96 w-80 flex items-end">
+                    </NavLink>
+                </li>
+                <li className="bg-support bg-cover bg-[30%] h-96 w-80 transition-all hover:bg-gradient-to-b from-slate-300 to-slate-700">
+                    <NavLink to="/" className="h-full w-full flex items-end transition-all hover:items-center hover:justify-center hover:text-center">
                         <h3 className="text-lg text-white font-medium p-8 w-[73%]">
                             Admin & Customer Support
                         </h3>
-                    </li>
-                </NavLink>
+                    </NavLink>
+                </li>
             </ul>
         </section>
     )
