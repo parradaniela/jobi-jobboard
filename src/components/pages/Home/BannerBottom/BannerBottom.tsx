150f5b1dca7ac0212a7d63e781e5b9260f349549
DanielaParra <dparralp396@gmail.com>
2023-04-13 15:27:36 -0400

    Mobile styles for bottom banner

--- src/components/pages/Home/BannerBottom/BannerBottom.tsx
+++ src/components/pages/Home/BannerBottom/BannerBottom.tsx
@@ -2,12 +2,12 @@ import Anchor from "../../../Global/Buttons/Anchor"
 
 const BannerBottom = () => {
     return (
-        <section className="bg-bottom-banner bg-cover bg-center py-16 font-gordita">
-            <div className="wrapper flex justify-evenly gap-8">
-                <h2 className="text-6xl text-white w-1/2 capitalize">A complete job portal for everyone</h2>
-                <div className="py-8">
+        <section className="bg-bottom-banner bg-cover bg-center py-8 font-gordita md:py-16">
+            <div className="wrapper-sm md:wrapper flex flex-col items-center gap-4 text-center md:text-left md:flex-row md:justify-evenly">
+                <h2 className="text-4xl text-white capitalize md:text-6xl lg:w-1/3">A complete job portal for everyone</h2>
+                <div className="md:py-8">
                     <p className="text-lg text-white">Find your dream job & earn from all worldwide leading brands</p>
-                    <div className="flex justify-center items-center gap-4 py-8 ">
+                    <div className="flex flex-col justify-center items-center gap-4 py-8 md:flex-row">
                         <Anchor style="btn-outline" corners="rounded" url="/">Looking for a job?</Anchor>
                         <Anchor style="btn-green" corners="rounded" url="/">Start Hiring</Anchor>
                     </div>
