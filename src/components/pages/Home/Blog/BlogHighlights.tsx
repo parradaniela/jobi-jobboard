acb5d5af16630ffac9f7fe1434fccbe75aa4b312
DanielaParra <dparralp396@gmail.com>
2023-04-13 15:00:25 -0400

    Mobile styles for home blog section, added ionicon arrows

--- src/components/pages/Home/Blog/BlogHighlights.tsx
+++ src/components/pages/Home/Blog/BlogHighlights.tsx
@@ -9,9 +9,9 @@ import articleImgThree from "../../../../assets/index/photos/photo-laptop-corinn
 
 const BlogHighlights = () => {
   return (
-    <section className="wrapper font-gordita py-8">
-      <div className="flex items-center justify-between pb-8">
-        <h2 className="text-6xl text-jobi-green-dark">Jobi Guides</h2>
+    <section className="wrapper-sm md:wrapper font-gordita py-12">
+      <div className="flex flex-col gap-4 items-center justify-between pb-8 md:flex-row">
+        <h2 className="text-4xl md:text-6xl text-jobi-green-dark">Jobi Guides</h2>
         <NavLink
           to="/"
           className="text-sm text-jobi-green-bright py-2 border-b-2 border-b-jobi-green-bright transition-all hover:text-jobi-green-dark hover:border-b-jobi-green-dark focus:text-jobi-green-dark focus:border-b-jobi-green-dark"
@@ -19,7 +19,7 @@ const BlogHighlights = () => {
           Explore More &rsaquo;
         </NavLink>
       </div>
-      <div className="flex justify-evenly gap-8 py-8">
+      <div className="flex flex-wrap gap-4 py-8 lg:justify-evenly">
         <HighlightArticle img={articleImgOne} alt="Woman working at home from her laptop, sitting on the ground on top of a bright red carpet" />
         <HighlightArticle img={articleImgTwo} alt="A young businessman dressed in a suit walks down an alleyway, smiling and looking upwards" />
         <HighlightArticle img={articleImgThree} alt="Overhead angle of a woman typing on a laptop on top of a round white table. A small plant, a pair of glasses, two pencils and a face-down mobile phone are visible" />
--- src/components/pages/Home/Blog/HighlightArticle.tsx
+++ src/components/pages/Home/Blog/HighlightArticle.tsx
@@ -1,4 +1,8 @@
+//React imports
 import { NavLink } from "react-router-dom"
+//ionicons
+import { IonIcon } from "@ionic/react"
+import { arrowForwardOutline } from "ionicons/icons"
 
 type Props = {
     img: string,
@@ -7,15 +11,21 @@ type Props = {
 
 const HighlightArticle = ({img, alt}: Props) => {
     return (
-        <article className="w-1/4">
-            <NavLink to="/" className="flex flex-col gap-4 p-4 transition-all hover:bg-jobi-green-light hover:-translate-y-4 group">
-                <img src={img} alt={alt} />
-                <p className="text-xs font-gorditaLight">22 July, 2022</p>
-                <h3 className="text-lg font-medium text-jobi-green-dark">Challenge yourself and win the future</h3>
-                {/* TODO: Create a function to truncate text properly */}
-                <p className="text-base font-gorditaLight">This response is important for our ability to learn from mistakes, but words ...</p>
-                <span className="sr-only">Click to read this blog post</span>
-                <span className="group-hover:translate-x-[95%] group-hover:text-lg transition-all">&rarr;</span>
+        <article className="lg:w-1/4">
+            <NavLink to="/" className="flex flex-col gap-4 p-4 transition-all hover:bg-jobi-green-light hover:-translate-y-4 group sm:flex-row lg:flex-col">
+                <div className="sm:w-2/3 lg:w-full">
+                    <img src={img} alt={alt} />
+                </div>
+                <div className="flex flex-col justify-evenly gap-2">
+                    <p className="text-xs font-gorditaLight">22 July, 2022</p>
+                    <h3 className="text-lg font-medium text-jobi-green-dark">Challenge yourself and win the future</h3>
+                    {/* TODO: Create a function to truncate text properly */}
+                    <p className="text-base font-gorditaLight">This response is important for our ability to learn from mistakes, but words ...</p>
+                    <span className="sr-only">Click to read this blog post</span>
+                    <span className="group-hover:translate-x-[95%] transition-all">
+                        <IonIcon icon={arrowForwardOutline} />
+                    </span>
+                </div>
             </NavLink>
         </article>
     )
