c0019baec94d51af30ed47f8013662836bf4e664
DanielaParra <dparralp396@gmail.com>
2023-04-16 15:07:00 -0400

    Added styles for mobile hamburger menu

--- src/components/pages/Home/Header/Nav.tsx
+++ src/components/pages/Home/Header/Nav.tsx
@@ -1,29 +1,56 @@
+//react imports
+import { useState } from "react"
+//component imports
 import Button from "../../../Global/Buttons/Button"
 import ListItemLinks from "../../../Global/LILinks/ListItemLinks"
 import LogoHorizontal from "../../../Logo/LogoHorizontal"
+//ionicons
+import { IonIcon } from "@ionic/react"
+import { closeCircleOutline, menuOutline } from "ionicons/icons"
 
 const Nav = () => {
+    const [open, setOpen] = useState(false)
+
+    const toggleMobileMenu = () => setOpen(!open)
     return (
-        <nav className="flex items-center justify-evenly font-gordita text-white py-6">
-            <div>
-                <LogoHorizontal alt={"Jobi Home Page"}>
-                    <h1 className="sr-only">Jobi</h1>
-                </LogoHorizontal>
-            </div>
+        <>
+            <button className="ml-[75%] mt-4 text-white text-6xl transition-all border-transparent rounded-lg p-1 border-2 hover:border-white focus:border-white md:hidden " onClick={toggleMobileMenu}>
+                <span className="sr-only">Open navigation menu</span>
+                <IonIcon icon={menuOutline} />
+            </button>
+            <nav
+                className={
+                    open
+                        ? `fixed top-0 left-0 transition-all duration-500 flex flex-col justify-evenly items-center gap-8 font-gordita min-h-screen w-full text-white bg-jobi-black py-6 
+                        md:static md:flex-row md:min-h-fit md:gap-0 md:bg-transparent`
+                        : `fixed top-0 left-full transition-all duration-500 flex flex-col justify-evenly items-center gap-8 font-gordita min-h-screen w-full invisible text-white bg-jobi-black py-6 
+                        md:static md:flex-row md:min-h-fit md:gap-0 md:bg-transparent md:visible`
+                }
+            >
+                <div className="flex justify-evenly items-center w-full md:w-auto">
+                    <LogoHorizontal alt="Jobi Home Page">
+                        <h1 className="sr-only">Jobi</h1>
+                    </LogoHorizontal>
+                    <button className="text-white text-6xl transition-all border-transparent rounded-lg p-1 border-2 hover:text-jobi-green-lime focus:text-jobi-green-lime md:hidden" onClick={toggleMobileMenu}>
+                        <span className="sr-only">Close navigation menu</span>
+                        <IonIcon icon={closeCircleOutline} />
+                    </button>
+                </div>
 
-            <ul className="flex text-sm gap-8 ">
-                <ListItemLinks url="/" text="Home" extraClasses="hover:text-jobi-green-lime focus-within:text-jobi-green-lime"/>
-                <ListItemLinks url="job-list" text="Jobs" extraClasses="hover:text-jobi-green-lime focus-within:text-jobi-green-lime"/>
-                <ListItemLinks url="/" text="Explore" extraClasses="hover:text-jobi-green-lime focus-within:text-jobi-green-lime"/>
-                <ListItemLinks url="/" text="Category" extraClasses="hover:text-jobi-green-lime focus-within:text-jobi-green-lime"/>
-                <ListItemLinks url="/" text="Pages" extraClasses="hover:text-jobi-green-lime focus-within:text-jobi-green-lime"/>
-            </ul>
+                <ul className="flex flex-col text-2xl text-center gap-8 md:flex-row md:text-base md:text-left" onClick={toggleMobileMenu}>
+                    <ListItemLinks url="/" text="Home" extraClasses="hover:text-jobi-green-lime focus-within:text-jobi-green-lime" />
+                    <ListItemLinks url="job-list" text="Jobs" extraClasses="hover:text-jobi-green-lime focus-within:text-jobi-green-lime" />
+                    <ListItemLinks url="/" text="Explore" extraClasses="hover:text-jobi-green-lime focus-within:text-jobi-green-lime" />
+                    <ListItemLinks url="/" text="Category" extraClasses="hover:text-jobi-green-lime focus-within:text-jobi-green-lime" />
+                    <ListItemLinks url="/" text="Pages" extraClasses="hover:text-jobi-green-lime focus-within:text-jobi-green-lime" />
+                </ul>
 
-            <div className="flex gap-8">
-                <Button style="btn-outline" corners="rounded-full">Login/Sign Up</Button>
-                <Button style="btn-green" corners="rounded-full">Post a job</Button>
-            </div>
-        </nav>
+                <div className="flex flex-col gap-8 md:flex-row">
+                    <Button style="btn-outline" corners="rounded-full">Login/Sign Up</Button>
+                    <Button style="btn-green" corners="rounded-full">Post a job</Button>
+                </div>
+            </nav>
+        </>
     )
 }
 
