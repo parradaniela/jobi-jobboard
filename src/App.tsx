1d6b38485008abc08cba5651d1c08e26dcc23c26
DanielaParra <dparralp396@gmail.com>
2023-05-07 17:33:35 -0400

    Adding router temporarily back to App.tsx to troubleshoot
    Moving it to its own component is causing errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 //router imports
-import { RouterProvider } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 //context import
 import DatabaseProvider from "./context/DatabaseContext";
-import { router } from "./router/router";
+//layout imports
+import ErrorLayout from "./layouts/ErrorLayout";
+import JobsLayout from "./layouts/JobsLayout";
+import RootLayout from "./layouts/RootLayout";
+//component imports
+import WIP from "./views/Error/WIP";
+import Home from "./views/Home/Home";
 
 function App() {
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="jobs" element={<JobsLayout />} >
+          <Route path="wip" element={<WIP />} />
+        </Route>
+        <Route path="*" element={<ErrorLayout />} />
+      </>
+    )
+  )
   return (
     <DatabaseProvider>
       <RouterProvider router={router} />
